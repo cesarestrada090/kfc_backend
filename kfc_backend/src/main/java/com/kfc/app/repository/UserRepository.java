0044db4054820b8f14464a70c8ec7a72e62d2820
Estrada <cesar.estrada@sap.com>
2024-05-27 17:21:18 -0500

    Fix for Person and User

--- kfc_backend/src/main/java/com/kfc/app/repository/UserRepository.java
+++ kfc_backend/src/main/java/com/kfc/app/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findUserByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsernameAndPassword(String username, String password);
 
     Optional<User> findByUsername(String username);
 }
--- kfc_backend/src/main/java/com/kfc/app/service/impl/UserServiceImpl.java
+++ kfc_backend/src/main/java/com/kfc/app/service/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import com.kfc.app.exception.DuplicatedUserException;
 import com.kfc.app.exception.InvalidPasswordException;
 import com.kfc.app.exception.NotFoundException;
 import com.kfc.app.repository.PersonRepository;
-import com.kfc.app.service.PersonService;
 import com.kfc.app.util.MapperUtil;
 import com.kfc.app.util.PaginationUtil;
 import com.kfc.app.dto.UserDto;
@@ -18,9 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -59,27 +56,18 @@ public class UserServiceImpl implements UserService {
         if(currentUser.isEmpty()){
             throw new NotFoundException("User does not exists: " + userDto);
         }
-        User user = currentUser.get();
-        
-        // if you include username different from actual, 
-        // we need to validate that the new one should not exist
-        if(userDto.hasDifferentUserName(currentUser.get().getUsername()) ){
-            if(usernameAlreadyExists(userDto.getUsername())){
-                throw new DuplicatedUserException("Username duplicated for " + userDto.getUsername());    
-            }
-            user.setUsername(userDto.getUsername());
-        }
-        
-        user.setPassword(userDto.getPassword());
-        user.setUpdatedAt(LocalDateTime.now());
-        userRepository.save(user);
-        return MapperUtil.map(user, userDto.getClass());
+        User userEntity = currentUser.get();
+        Person personEntity = userEntity.getPerson();
+        preparePersonEntity(userDto.getPerson(), personEntity);
+        prepareUserEntity(userDto, userEntity);
+        userRepository.save(userEntity);
+        return MapperUtil.map(userEntity, userDto.getClass());
     }
 
     @Override
     public UserDto getByUsernameAndPassword(UserDto userDto) {
         Optional<User> user = userRepository.
-                findUserByUsernameAndPassword(userDto.getUsername(), userDto.getPassword());
+                findByUsernameAndPassword(userDto.getUsername(), userDto.getPassword());
         if(user.isEmpty()) {
             throw new InvalidPasswordException("Username: " + userDto.getUsername());
         }
@@ -108,5 +96,31 @@ public class UserServiceImpl implements UserService {
             throw  new NotFoundException("Users does not exists");
         }
         return PaginationUtil.prepareResultWrapper(users, UserDto.class);
+    }
+    private void prepareUserEntity(UserDto userDto, User userEntity) {
+        // if you include username different from actual, 
+        // we need to validate that the new one should not exist
+        if(userDto.hasDifferentUserName(userEntity.getUsername()) ){
+            if(usernameAlreadyExists(userDto.getUsername())){
+                throw new DuplicatedUserException("Username duplicated for " + userDto.getUsername());
+            }
+            userEntity.setUsername(userDto.getUsername());
+        }
+
+        userEntity.setPassword(userDto.getPassword());
+        userEntity.setUpdatedAt(LocalDateTime.now());
+    }
+
+    private void preparePersonEntity(PersonDto personDto, Person personEntity) {
+        if(personDto.hasDifferentDocumentNumber(personEntity.getDocumentNumber()) ){
+            if(personRepository.findByDocumentNumber(personDto.getDocumentNumber()).isPresent()){
+                throw new DuplicatedUserException("Document Number duplicated for " + personDto.getDocumentNumber());
+            }
+            personEntity.setDocumentNumber(personDto.getDocumentNumber());
+        }
+        personEntity.setLastName(personDto.getLastName());
+        personEntity.setFirstName(personDto.getFirstName());
+        personEntity.setPhoneNumber(personDto.getPhoneNumber());
+        personEntity.setEmail(personDto.getEmail());
     }
 }
