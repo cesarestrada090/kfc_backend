b784cd7ba5feef6a8e6ddc49ed47f4f24c2d70aa
Estrada <cesar.estrada@sap.com>
2024-06-07 14:26:52 -0500

    Add detail repo

--- kfc_backend/src/main/java/com/kfc/app/service/MaintenanceDetailService.java
+++ kfc_backend/src/main/java/com/kfc/app/service/MaintenanceDetailService.java
@@ -1,11 +1,10 @@
 package com.kfc.app.service;
 
 import com.kfc.app.dto.MaintenanceDetailDto;
-import com.kfc.app.dto.MaintenanceDetailDto;
 import com.kfc.app.dto.ResultPageWrapper;
 import org.springframework.data.domain.Pageable;
 
-public interface MaintenanceServiceDetail {
+public interface MaintenanceDetailService {
     MaintenanceDetailDto save(MaintenanceDetailDto dto);
     MaintenanceDetailDto update(Integer id, MaintenanceDetailDto dto);
     ResultPageWrapper<MaintenanceDetailDto> findByOrganizationId(Integer orgId, Pageable paging);
--- kfc_backend/src/main/java/com/kfc/app/service/impl/MaintenanceDetailServiceImpl.java
+++ kfc_backend/src/main/java/com/kfc/app/service/impl/MaintenanceDetailServiceImpl.java
@@ -1,17 +1,13 @@
 package com.kfc.app.service.impl;
 
+import com.kfc.app.dto.MaintenanceDetailDto;
 import com.kfc.app.dto.MaintenanceDto;
 import com.kfc.app.dto.ResultPageWrapper;
-import com.kfc.app.entities.Maintenance;
-import com.kfc.app.entities.Unit;
-import com.kfc.app.entities.User;
-import com.kfc.app.entities.Workshop;
+import com.kfc.app.entities.*;
 import com.kfc.app.exception.NotFoundException;
+import com.kfc.app.repository.MaintenanceDetailRepository;
 import com.kfc.app.repository.MaintenanceRepository;
-import com.kfc.app.service.MaintenanceService;
-import com.kfc.app.service.UnitService;
-import com.kfc.app.service.UserService;
-import com.kfc.app.service.WorkshopService;
+import com.kfc.app.service.*;
 import com.kfc.app.util.MapperUtil;
 import com.kfc.app.util.PaginationUtil;
 import org.springframework.data.domain.Page;
@@ -23,72 +19,50 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
-public class MaintenanceDetailServiceImpl implements MaintenanceService {
+public class MaintenanceDetailServiceImpl implements MaintenanceDetailService {
     
     private final MaintenanceRepository maintenanceRepository;
-    private final UnitService unitService;
-    private final UserService userService;
-    private final WorkshopService workshopService;
+    private final MaintenanceDetailRepository maintenanceDetailRepository;
 
     public MaintenanceDetailServiceImpl(MaintenanceRepository maintenanceRepository,
-                                        WorkshopService workshopService,
-                                        UnitService unitService,
-                                        UserService userService) {
+                                        MaintenanceDetailRepository maintenanceDetailRepository) {
         this.maintenanceRepository = maintenanceRepository;
-        this.workshopService = workshopService;
-        this.unitService = unitService;
-        this.userService = userService;
+        this.maintenanceDetailRepository = maintenanceDetailRepository;
     }
 
     @Override
     @Transactional
-    public MaintenanceDto save(MaintenanceDto dto) {
-        Unit unit = unitService.getUnitEntityById(dto.getUnit().getId());
-        Workshop workshop = workshopService.getWorkshopEntityById(dto.getWorkshop().getId());
-        User user = userService.getUserEntityById(dto.getCreatedBy().getId());
-        Maintenance maintenance = new Maintenance();
-        maintenance.setUnit(unit);
-        maintenance.setWorkshop(workshop);
-        maintenance.setDescription(dto.getDescription());
-        maintenance.setMaintenanceDate(dto.getMaintenanceDate());
-        maintenance.setCompleted(dto.isCompleted());
-        maintenance.setUpdatedAt(LocalDateTime.now());
-        maintenance.setCreatedAt(LocalDateTime.now());
-        maintenance.setLastUpdatedBy(user);
-        maintenance.setCreatedBy(user);
-        maintenance = maintenanceRepository.save(maintenance);
-        return MapperUtil.map(maintenance, MaintenanceDto.class);
+    public MaintenanceDetailDto save(MaintenanceDetailDto dto) {
+        MaintenanceDetail maintenanceDetail = new MaintenanceDetail();
+        maintenanceDetail.setQuantity(dto.getQuantity());
+        maintenanceDetail.setDescription(dto.getDescription());
+        maintenanceDetail = maintenanceDetailRepository.save(maintenanceDetail);
+        return MapperUtil.map(maintenanceDetail, MaintenanceDetailDto.class);
     }
 
     @Override
-    public MaintenanceDto update(Integer id, MaintenanceDto dto) {
-        Optional<Maintenance> maintenanceOpt = maintenanceRepository.findById(id);
+    public MaintenanceDetailDto update(Integer id, MaintenanceDetailDto dto) {
+        Optional<MaintenanceDetail> maintenanceOpt = maintenanceDetailRepository.findById(id);
         if (maintenanceOpt.isEmpty()) {
             throw new NotFoundException("Maintenance not found with id: " + id);
         }
-        Maintenance maintenance = maintenanceOpt.get();
-        User user = userService.getUserEntityById(dto.getCreatedBy().getId());
-        maintenance.setDescription(dto.getDescription());
-        maintenance.setMaintenanceDate(dto.getMaintenanceDate());
-        maintenance.setCompleted(dto.isCompleted());
-        maintenance.setUpdatedAt(LocalDateTime.now());
-        maintenance.setCreatedAt(LocalDateTime.now());
-        maintenance.setLastUpdatedBy(user);
-        maintenance.setCreatedBy(user);
-        maintenance = maintenanceRepository.save(maintenance);
-        return MapperUtil.map(maintenance, MaintenanceDto.class);
+        MaintenanceDetail maintenanceDetail = maintenanceOpt.get();
+        maintenanceDetail.setQuantity(dto.getQuantity());
+        maintenanceDetail.setDescription(dto.getDescription());
+        maintenanceDetail = maintenanceDetailRepository.save(maintenanceDetail);
+        return MapperUtil.map(maintenanceDetail, MaintenanceDetailDto.class);
     }
 
     @Override
-    public ResultPageWrapper<MaintenanceDto> findByOrganizationId(Integer orgId, Pageable paging) {
+    public ResultPageWrapper<MaintenanceDetailDto> findByOrganizationId(Integer orgId, Pageable paging) {
         Page<Maintenance> maintenances = maintenanceRepository.findMaintenanceByOrganizationId(orgId, paging);
-        return PaginationUtil.prepareResultWrapper(maintenances, MaintenanceDto.class);
+        return PaginationUtil.prepareResultWrapper(maintenances, MaintenanceDetailDto.class);
     }
 
     @Override
-    public MaintenanceDto getById(Integer id) {
+    public MaintenanceDetailDto getById(Integer id) {
         return maintenanceRepository.findById(id)
-                .map(warehouse -> MapperUtil.map(warehouse, MaintenanceDto.class))
+                .map(warehouse -> MapperUtil.map(warehouse, MaintenanceDetailDto.class))
                 .orElseThrow(() -> new NotFoundException("Maintenance not found with id: " + id));
     }
 }
